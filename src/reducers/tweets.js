3af5235244c451fb271267c2907edff59fe20094
Akram A <akram.f11@gmail.com>
2019-05-29 21:02:38 +0200

    added toggleLike reducer

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,6 @@
 import { RECEIVE_TWEETS } from '../actions/tweets'
+import {TOGGLE_LIKE} from '../actions/tweets'
+import { Z_FILTERED } from 'zlib';
 
 export default function tweets(state = {}, action) {
     switch (action.type) {
@@ -6,6 +8,16 @@ export default function tweets(state = {}, action) {
             return {
                 ...state,
                 ...action.tweets
+            }
+        case TOGGLE_LIKE:
+            return {
+                ...state,
+                [action.id]: {
+                    ...[action.id],
+                    likes: action.hasLiked === true ? 
+                    state[action.id].likes.filter((uid) => uid !== action.autheUser)
+                    : state[action.id].like.concat(action.autheUser)
+                }
             }
         default: return state
     }
